3e92c35a85e83a741a3bdb0b2afc78b3300ce0fb
Mark Spanbroek <mark@spanbroek.net>
2021-04-12 15:51:25 +0200

    Rename chaining and indexing operators
    Chaining operator is now `.?`
    Indexing operator is now `.?[]`
    
    Reason: interoperability with the binding `=?` operator.
    Chaining and indexing now have lower operator precedence
    than binding, ensuring that expressions such as `if a =? b.?c`
    work as expected.

--- questionable/chaining.nim
+++ questionable/chaining.nim
@@ -1,39 +1,40 @@
+import std/options
 import std/macros
 
-template `?.`*(option: typed, identifier: untyped{nkIdent}): untyped =
-  # chain is of shape: option?.identifier
+template `.?`*(option: typed, identifier: untyped{nkIdent}): untyped =
+  # chain is of shape: option.?identifier
   option ->? option.unsafeGet.identifier
 
-macro `?.`*(option: typed, infix: untyped{nkInfix}): untyped =
-  # chain is of shape: option?.left `operator` right
+macro `.?`*(option: typed, infix: untyped{nkInfix}): untyped =
+  # chain is of shape: option.?left `operator` right
   let left = infix[1]
-  infix[1] = quote do: `option`?.`left`
+  infix[1] = quote do: `option`.?`left`
   infix
 
-macro `?.`*(option: typed, bracket: untyped{nkBracketExpr}): untyped =
-  # chain is of shape: option?.left[right]
+macro `.?`*(option: typed, bracket: untyped{nkBracketExpr}): untyped =
+  # chain is of shape: option.?left[right]
   let left = bracket[0]
-  bracket[0] = quote do: `option`?.`left`
+  bracket[0] = quote do: `option`.?`left`
   bracket
 
-macro `?.`*(option: typed, dot: untyped{nkDotExpr}): untyped =
-  # chain is of shape: option?.left.right
+macro `.?`*(option: typed, dot: untyped{nkDotExpr}): untyped =
+  # chain is of shape: option.?left.right
   let left = dot[0]
-  dot[0] = quote do: `option`?.`left`
+  dot[0] = quote do: `option`.?`left`
   dot
 
-macro `?.`*(option: typed, call: untyped{nkCall}): untyped =
+macro `.?`*(option: typed, call: untyped{nkCall}): untyped =
   let procedure = call[0]
   if call.len == 1:
-    # chain is of shape: option?.procedure()
-    quote do: `option`?.`procedure`
+    # chain is of shape: option.?procedure()
+    quote do: `option`.?`procedure`
   elif procedure.kind == nnkDotExpr:
-    # chain is of shape: option?.left.right(arguments)
+    # chain is of shape: option.?left.right(arguments)
     let (left, right) = (procedure[0], procedure[1])
     call[0] = right
-    call.insert(1, quote do: `option`?.`left`)
+    call.insert(1, quote do: `option`.?`left`)
     call
   else:
-    # chain is of shape: option?.procedure(arguments)
+    # chain is of shape: option.?procedure(arguments)
     call.insert(1, quote do: `option`.unsafeGet)
     quote do: `option` ->? `call`
--- questionable/indexing.nim
+++ questionable/indexing.nim
@@ -1,7 +1,7 @@
 import std/macros
 
-macro `?`*(expression: typed, brackets: untyped{nkBracket}): untyped =
-  # chain is of shape: expression?[index]
+macro `.?`*(expression: typed, brackets: untyped{nkBracket}): untyped =
+  # chain is of shape: expression.?[index]
   let index = brackets[0]
   quote do:
     type T = typeof(`expression`[`index`])
@@ -9,33 +9,3 @@ macro `?`*(expression: typed, brackets: untyped{nkBracket}): untyped =
       `expression`[`index`].some
     except KeyError:
       T.none
-
-macro `?`*(expression: typed, infix: untyped{nkInfix}): untyped =
-  # chain is of shape: expression?left `operator` right
-  let left = infix[1]
-  infix[1] = quote do: `expression`?`left`
-  infix
-
-macro `?`*(expression: typed, bracket: untyped{nkBracketExpr}): untyped =
-  # chain is of shape: expression?left[right]
-  let left = bracket[0]
-  bracket[0] = quote do: `expression`?`left`
-  bracket
-
-macro `?`*(expression: typed, dot: untyped{nkDotExpr}): untyped =
-  # chain is of shape: expression?left.right
-  let left = dot[0]
-  dot[0] = quote do: `expression`?`left`
-  dot
-
-macro `?`*(expression: typed, call: untyped{nkCall}): untyped =
-  let procedure = call[0]
-  if procedure.kind == nnkDotExpr:
-    # chain is of shape: expression?left.right(arguments)
-    let (left, right) = (procedure[0], procedure[1])
-    call[0] = right
-    call.insert(1, quote do: `expression`?`left`)
-    call
-  else:
-    call.expectKind(nnkBracketExpr)
-    nil
