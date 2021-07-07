8feb684574b7c4659c1b60b8ba8ab7e7b08a52ba
Mark Spanbroek <mark@spanbroek.net>
2021-07-07 09:19:44 +0200

    Fix "unused variable" warnings

--- questionable/binding.nim
+++ questionable/binding.nim
@@ -7,12 +7,12 @@ proc option[T](option: Option[T]): Option[T] =
 template bindLet(name, expression): bool =
   let option = expression.option
   const default = typeof(option.unsafeGet()).default
-  let name = if option.isSome: option.unsafeGet() else: default
+  let name {.used.} = if option.isSome: option.unsafeGet() else: default
   option.isSome
 
 template bindVar(name, expression): bool =
   let option = expression.option
-  var name : typeof(option.unsafeGet())
+  var name {.used.} : typeof(option.unsafeGet())
   if option.isSome:
     name = option.unsafeGet()
   option.isSome
