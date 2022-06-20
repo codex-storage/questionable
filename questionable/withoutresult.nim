9016c9f65caa3958574ddcff48dbd1dbd0cd54ff
Mark Spanbroek <mark@spanbroek.net>
2022-06-20 17:36:24 +0200

    Fix: `without` statement with error works in generic code

--- questionable/withoutresult.nim
+++ questionable/withoutresult.nim
@@ -1,39 +1,12 @@
-import std/macros
-import ./binding
 import ./without
+import ./private/binderror
 
-macro replaceInfix(expression, operator, replacement): untyped =
-  ## Replaces an infix operator in an expression. The AST of the expression is
-  ## traversed to find and replace all instances of the operator.
-
-  proc replace(expression, operator, replacement: NimNode): NimNode =
-    if expression.kind == nnkInfix and eqIdent(expression[0], operator):
-      expression[0] = replacement
-      expression[2] = replace(expression[2], operator, replacement)
-    else:
-      for i in 0..<expression.len:
-        expression[i] = replace(expression[i], operator, replacement)
-    expression
-
-  replace(expression, operator, replacement)
-
-template without*(condition, errorname, body) =
+template without*(condition, errorname, body): untyped =
   ## Used to place guards that ensure that a Result contains a value.
   ## Exposes error when Result does not contain a value.
 
   var error: ref CatchableError
 
-  # override =? operator such that it stores the error if there is one
-  template `override=?`(name, expression): bool {.gensym, used.} =
-    let optional = expression
-    when optional is Result:
-      if optional.isFailure:
-        error = optional.error
-    when optional is Option:
-      if optional.isNone:
-        error = newException(ValueError, "Option is set to `none`")
-    name =? optional
-
-  without replaceInfix(condition, `=?`, `override=?`):
+  without captureBindError(error, condition):
     template errorname: ref CatchableError = error
     body
