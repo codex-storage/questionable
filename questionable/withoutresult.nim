c8a7b189ea889cae1816a8c5b2c370b6ca4cca78
Mark Spanbroek <mark@spanbroek.net>
2022-08-10 11:11:45 +0200

    Fix: disregard early symbol lookup for error variable

--- questionable/withoutresult.nim
+++ questionable/withoutresult.nim
@@ -2,11 +2,31 @@ import std/macros
 import ./without
 import ./private/binderror
 
-macro without*(condition, errorname, body): untyped =
+proc undoSymbolResolution(expression, ident: NimNode): NimNode =
+  ## Finds symbols in the expression that match the `ident` and replaces them
+  ## with `ident`, effectively undoing any symbol resolution that happened
+  ## before.
+
+  const symbolKinds = {nnkSym, nnkOpenSymChoice, nnkClosedSymChoice}
+
+  if expression.kind in symbolKinds and eqIdent($expression, $ident):
+    return ident
+
+  for i in 0..<expression.len:
+    expression[i] = undoSymbolResolution(expression[i], ident)
+
+  expression
+
+macro without*(condition, errorname, body: untyped): untyped =
   ## Used to place guards that ensure that a Result contains a value.
   ## Exposes error when Result does not contain a value.
 
   let errorIdent = ident $errorname
+
+  # Nim's early symbol resolution might have picked up a symbol with the
+  # same name as our error variable. We need to undo this to make sure that our
+  # error variable is seen.
+  let body = body.undoSymbolResolution(errorIdent)
 
   quote do:
     var error: ref CatchableError
