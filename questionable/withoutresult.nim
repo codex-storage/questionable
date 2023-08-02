086fd4a8db1cb21cb30d9d3279bb83886d97ac30
Mark Spanbroek <mark@spanbroek.net>
2023-08-02 15:13:32 +0200

    Better compilation error when calling `without` with wrong parameter

--- questionable/withoutresult.nim
+++ questionable/withoutresult.nim
@@ -2,12 +2,13 @@ import std/macros
 import ./without
 import ./private/binderror
 
+const symbolKinds = {nnkSym, nnkOpenSymChoice, nnkClosedSymChoice}
+const identKinds = {nnkIdent} + symbolKinds
+
 proc undoSymbolResolution(expression, ident: NimNode): NimNode =
   ## Finds symbols in the expression that match the `ident` and replaces them
   ## with `ident`, effectively undoing any symbol resolution that happened
   ## before.
-
-  const symbolKinds = {nnkSym, nnkOpenSymChoice, nnkClosedSymChoice}
 
   if expression.kind in symbolKinds and eqIdent($expression, $ident):
     return ident
@@ -21,6 +22,9 @@ proc undoSymbolResolution(expression, ident: NimNode): NimNode =
 macro without*(condition, errorname, body: untyped): untyped =
   ## Used to place guards that ensure that a Result contains a value.
   ## Exposes error when Result does not contain a value.
+
+  if errorname.kind notin identKinds:
+    error("expected an identifier, got " & errorname.repr, errorname)
 
   let errorIdent = ident $errorname
 
