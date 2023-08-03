e56cf86c4a089c78a1b7c3005f13343bfbbe3b48
Mark Spanbroek <mark@spanbroek.net>
2023-08-03 14:51:31 +0200

    Indexing of strings and sequences should not catch Defect
    Catching a Defect does not always work, depending on how
    it's compiled.

--- questionable/indexing.nim
+++ questionable/indexing.nim
@@ -1,7 +1,16 @@
 import std/macros
 
-when (NimMajor, NimMinor) < (1, 4):
-  type IndexDefect = IndexError
+macro `.?`*(expression: seq | string, brackets: untyped{nkBracket}): untyped =
+  # chain is of shape: (seq or string).?[index]
+  let index = brackets[0]
+  quote do:
+    block:
+      type T = typeof(`expression`[`index`])
+      let evaluated = `expression`
+      if `index` < evaluated.len:
+        evaluated[`index`].some
+      else:
+        T.none
 
 macro `.?`*(expression: typed, brackets: untyped{nkBracket}): untyped =
   # chain is of shape: expression.?[index]
@@ -12,6 +21,4 @@ macro `.?`*(expression: typed, brackets: untyped{nkBracket}): untyped =
       try:
         `expression`[`index`].some
       except KeyError:
-        T.none
-      except IndexDefect:
         T.none
