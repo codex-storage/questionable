1f0afff48bf80ab1149a0957f9743f345bc14b71
Tomasz Bekas <tomasz.bekas@gmail.com>
2023-11-20 14:58:49 +0100

    Support for .?[] operator on openArrays (#52)
    * Support for .?[] operator on openArrays
    
    * Operator .?[] evaluates openArray expression only once
    
    * Fix for Nim 1.2.x
    
    ---------
    
    Co-authored-by: Mark Spanbroek <mark@spanbroek.net>

--- questionable/indexing.nim
+++ questionable/indexing.nim
@@ -1,16 +1,24 @@
 import std/macros
+import std/options
 
-macro `.?`*(expression: seq | string, brackets: untyped{nkBracket}): untyped =
-  # chain is of shape: (seq or string).?[index]
-  let index = brackets[0]
-  quote do:
-    block:
-      type T = typeof(`expression`[`index`])
-      let evaluated = `expression`
-      if `index` < evaluated.len:
-        evaluated[`index`].some
-      else:
-        T.none
+proc safeGet[T](expression: seq[T] | openArray[T], index: int): Option[T] =
+  if index >= expression.low and index <= expression.high:
+    expression[index].some
+  else:
+    T.none
+
+proc safeGet(expression: string, index: int): Option[char] =
+  if index >= expression.low and index <= expression.high:
+    expression[index].some
+  else:
+    char.none
+
+macro `.?`*(expression: seq | string | openArray, brackets: untyped{nkBracket}): untyped =
+    # chain is of shape: (seq or string or openArray).?[index]
+    let index = brackets[0]
+    quote do:
+      block:
+        safeGet(`expression`, `index`)
 
 macro `.?`*(expression: typed, brackets: untyped{nkBracket}): untyped =
   # chain is of shape: expression.?[index]
